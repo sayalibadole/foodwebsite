30f073ec245897e8afbe769ee677ce775a02885f
sayalibadole <badolesayali@gmail.com>
2023-08-20 23:09:41 -0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Pagenotfound from "./pages/Pagenotfound";
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename="/foodwebsite">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
