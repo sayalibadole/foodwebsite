c86b47df4aa2e1bd0687d0f97d7c17111462c292
sayalibadole <badolesayali@gmail.com>
2023-08-20 22:19:32 -0700

    food website

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import Pasta from "../images/pasta.jpg";
+import Pizza from "../images/pizza.jpg";
+import Pita from "../images/pita.jpg";
+import Sandwitch from "../images/sandwitch.jpg";
+import Salad from "../images/salad.jpg";
+import Icecream from "../images/icecream.jpg";
+
+export const MenuList = [
+  {
+    name: "Pasta",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Pasta,
+    price: 200,
+  },
+  {
+    name: "Pizza",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Pizza,
+    price: 250,
+  },
+  {
+    name: "Pita",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Pita,
+    price: 300,
+  },
+  {
+    name: "Sandwitch",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Sandwitch,
+    price: 100,
+  },
+  {
+    name: "Salad",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Salad,
+    price: 400,
+  },
+  {
+    name: "Icecream",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Icecream,
+    price: 500,
+  },
+];
